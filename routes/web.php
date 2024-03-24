c100a6b4681bc33d1461683148c0c6fe58ae569f
devskonnects <usman.islootech@gmail.com>
2024-03-24 23:20:30 +0500

    bladding with home page

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('client/home/main');
+});
+
+Route::get("home",function(){
+    dd('dd');
 });
