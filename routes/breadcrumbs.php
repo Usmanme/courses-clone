b3cd22ea1947362d2927bd2a2cf6f36a7a92dd03
Usman Yousaf <mani.yousaf98@gmailcom>
2024-03-27 19:23:35 +0500

    vuexy integration.

---
+++ routes/breadcrumbs.php
@@ -0,0 +1,10 @@
+<?php
+
+use Diglactic\Breadcrumbs\Breadcrumbs;
+use Diglactic\Breadcrumbs\Generator as BreadcrumbTrail;
+
+Breadcrumbs::for('dashboard', function (BreadcrumbTrail $trail) {
+    $trail->push("Dashboard", route('dashboard'));
+});
+
+
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,4 +20,16 @@ Route::get('/', function () {
 
 Route::get("single-course",function(){
     return view("front-end.course.single-course");
+});
+
+require __DIR__ . DIRECTORY_SEPARATOR . 'auth.php';
+
+Route::group([
+], function () {
+    Route::get('/login', function () {
+        return redirect()->route('login.view');
+    });
+
+    Route::get('dashboard', [DashboardController::class, 'dashboard'])->middleware('auth')->name('dashboard');
+    Route::get('cachew/flush', [DashboardController::class, 'cacheFlush'])->name('cache.flush');
 });
